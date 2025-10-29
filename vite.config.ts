8f80d44b53d81161c79658452d1e28f35dcde63f
DarkKingpro10 <jesusgogeta@outlook.com>
2025-10-28 20:24:33 -0600

    feat: Ya disponible para build en desktop y web

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// const isDesktop = process.env.VITE_TARGET === 'desktop'
+
 export default defineConfig({
   plugins: [
     react({
@@ -16,4 +18,6 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 5175,
   },
+  // Importante para Electron: que los assets en index.html sean relativos a file://
+  base: './',
 })
